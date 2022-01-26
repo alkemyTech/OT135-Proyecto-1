80abf8a98860db8b8e8a206d157bf4eac4b32808
Laureano Engulian <laureanoengulian@gmail.com>
2022-01-26 17:30:20 -0300

    sql query modified

--- sql/.sql
+++ sql/.sql
@@ -1,2 +1,3 @@
-SELECT * from moron_nacional_pampa, rio_cuarto_interamericana WHERE fechainscripccion BETWEEN '20200901' AND '20210201'
-SELECT * from rio_cuarto_interamericana WHERE inscription_dates AS fechainscripccion BETWEEN '20200901' AND '20210201'
+SELECT * FROM moron_nacional_pampa WHERE to_date(fechaiscripccion,'DD/MM/YYYY') BETWEEN '2020-09-01' AND '2021-02-01'
+UNION
+SELECT * FROM rio_cuarto_interamericana WHERE to_date(inscription_dates,'DD/Mon/YY') BETWEEN '2020-09-01' AND '2021-02-01'
