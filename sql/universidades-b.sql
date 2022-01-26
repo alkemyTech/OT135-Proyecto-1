9a5d95e4cea97226304e95f912583c6ff6bba31f
Juan Fernandez <juanfernandez_carc@hotmail.com>
2022-01-26 13:43:19 -0300

    query correction

--- sql/universidades-b.sql
+++ sql/universidades-b.sql
@@ -1,7 +1,7 @@
-SELECT universidad, carrera, name, sexo, fecha_nacimiento, codigo_postal, correo_electronico
+SELECT universidad as university, carrera as career, name as full_name, sexo as gender, fecha_nacimiento as date_of_birth, codigo_postal as postal_code, correo_electronico as email
 FROM flores_comahue
-WHERE fecha_de_inscripcion BETWEEN '2020/09/01' AND '2021/02/01'
+WHERE (universidad = 'Univ. Nacional Del Comahue') AND (fecha_de_inscripcion BETWEEN '2020/09/01' AND '2021/02/01')
 UNION
-SELECT universidad, carrera, nombre, sexo, fecha_nacimiento, localidad, email
+SELECT universidad as university, carrera as career, nombre as full_name, sexo as gender, fecha_nacimiento as date_of_birth, localidad as location, email as email
 FROM salvador_villa_maria
-WHERE fecha_de_inscripcion BETWEEN '2020/09/01' AND '2021/02/01'
+WHERE (universidad = 'Universidad Del Salvador') AND (fecha_de_inscripcion BETWEEN '2020/09/01' AND '2021/02/01')
