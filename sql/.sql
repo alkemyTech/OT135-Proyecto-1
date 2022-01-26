aaceeadd7ee0854e861a8a585798c49298de98a6
Laureano Engulian <laureanoengulian@gmail.com>
2022-01-26 18:01:06 -0300

    sql query update

--- sql/.sql
+++ sql/.sql
@@ -1,3 +1,9 @@
-SELECT * FROM moron_nacional_pampa WHERE to_date(fechaiscripccion,'DD/MM/YYYY') BETWEEN '2020-09-01' AND '2021-02-01'
-UNION
-SELECT * FROM rio_cuarto_interamericana WHERE to_date(inscription_dates,'DD/Mon/YY') BETWEEN '2020-09-01' AND '2021-02-01'
+SELECT universidad as university, carrerra AS  career, nombrre AS name, sexo AS gender,
+nacimiento AS age, codgoposstal AS postal_code, direccion AS location, eemail AS email, 
+fechaiscripccion AS inscription_date 
+FROM moron_nacional_pampa WHERE to_date(fechaiscripccion,'DD/MM/YYYY') BETWEEN '2020-09-01' AND '2021-02-01'
+UNION 
+SELECT univiersities as university, carrera as career, names as name, sexo as gender, fechas_nacimiento as age,
+'null' as postal_code, localidad as location, email, inscription_dates as inscription_date 
+from rio_cuarto_interamericana WHERE to_date(inscription_dates,'DD/Mon/YY') BETWEEN '2020-09-01' AND '2021-02-01'
+ORDER BY inscription_date
