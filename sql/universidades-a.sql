b43f52f25506655dc5fce10b6d52ebf4a1aa7d8c
Virginia Ambrosino <virginiaamb@gmail.com>
2022-01-26 16:57:56 -0300

    last motification

--- sql/universidades-a.sql
+++ sql/universidades-a.sql
@@ -1,7 +1,7 @@
 SELECT
 nombre,
 sexo,
-to_char("fecha_nacimiento"::date, 'YYYY-MM-DD') AS fecha_nacimiento,
+fecha_nacimiento,
 email,
 fecha_de_inscripcion,
 universidad,
@@ -10,7 +10,7 @@ localidad,
 'null' AS codigo_postal
 FROM public.salvador_villa_maria
 WHERE universidad = 'UNIVERSIDAD_NACIONAL_DE_VILLA_MARÍA'
-AND fecha_de_inscripcion BETWEEN '20-sep-01' AND '21-feb-01'
+AND TO_DATE(fecha_de_inscripcion,'DD/Mon/YY') BETWEEN '2020-09-01' AND '2021-02-01'
 UNION
 SELECT
 name AS nombre,
@@ -25,25 +25,3 @@ codigo_postal
 FROM public.flores_comahue
 WHERE universidad = 'UNIVERSIDAD DE FLORES'
 AND fecha_de_inscripcion BETWEEN '2020/09/01' AND '2021/02/01';
-
-
-select
-to_char("fecha_de_inscripcion"::date, 'YYYY-MM-DD') AS fecha_de_inscripcion
-from public.salvador_villa_maria
-where fecha_de_inscripcion BETWEEN '2020-09-01' AND '2021-02-01';
-
-select
-to_date("fecha_de_inscripcion"::date, 'YYYY-MM-DD') AS fecha_de_inscripcion
-from public.salvador_villa_maria
-where fecha_de_inscripcion BETWEEN '2020-09-01' AND '2021-02-01';
-
-select
-CAST("fecha_de_inscripcion"::date AS YYYY-MM-DD) AS fecha_de_inscripcion
-from public.salvador_villa_maria
-where fecha_de_inscripcion BETWEEN '2020-09-01' AND '2021-02-01';
-
-
-select * from public.salvador_villa_maria
-
-AND fecha_de_inscripcion BETWEEN '20-sep-01' AND '21-feb-01'
-
