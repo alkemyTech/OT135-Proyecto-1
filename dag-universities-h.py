85ef97ea54c949e47f73a535d0ffa58e54649e6e
Virginia Ambrosino <virginiaamb@gmail.com>
2022-01-26 19:28:12 -0300

    last version

--- dag-universities-h.py
+++ dag-universities-h.py
@@ -1,18 +1,16 @@
 from airflow import DAG
 from datetime import timedelta, datetime
 from airflow.operators.dummy import DummyOperator
-from airflow.operators import PythonOperator, BaseSQLOperator
-
 
 # instanciamos dag   
 with DAG(
     'dag-universities-h',
-    description='configuracion de dags para grupo de universidades h',
-    schedule_interval=timedelta(days=1, hours=1),
+    description='configuracion de dags para grupo de universidades',
+    schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 1, 26)
 ) as dag:
-    extract = BaseSQLOperator(task_id='extract')
-    transform = PythonOperator(task_id='transform')
-    load = DummyOperator(task_id='load')
+    extract = DummyOperator(task_id='extract from sql') # python operator
+    transform = DummyOperator(task_id='transform with pandas') # python operator
+    load = DummyOperator(task_id='load to s3') 
 
     extract >> transform >> load
