feb6b270212451d4fe9e44fedeee14076d9eed93
Virginia Ambrosino <virginiaamb@gmail.com>
2022-01-26 11:20:45 -0300

    select-format

--- sql/universidades-a.sql
+++ sql/universidades-a.sql
@@ -1,14 +1,27 @@
 SELECT
-v.nombre,
-v.sexo,
-v.fecha_nacimiento,
-v.email,
-v.fecha_de_inscripcion,
-v.universidad,
-v.carrera,
-v.localidad
-FROM public.salvador_villa_maria v
+nombre,
+sexo,
+fecha_nacimiento,
+email,
+fecha_de_inscripcion,
+universidad,
+carrera,
+localidad,
+'null' AS codigo_postal
+FROM public.salvador_villa_maria
 WHERE universidad = "villa maria"
-AND fecha_de_inscripcion BETWEEN ""
+AND fecha_de_inscripcion BETWEEN '2020/09/20' AND '2021/02/01'
 UNION
 SELECT
+name AS nombre,
+sexo,
+fecha_nacimiento,
+correo_electronico AS email,
+fecha_de_inscripcion,
+universidad,
+carrera,
+'null' AS localidad,
+codigo_postal
+FROM public.flores_comahue f
+WHERE universidad = "flores"
+AND fecha_de_inscripcion BETWEEN '2020/09/20' AND '2021/02/01';
