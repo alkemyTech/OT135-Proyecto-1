896af471f1e650b81b46986cc024b6a56b6967b0
Juan Fernandez <juanfernandez_carc@hotmail.com>
2022-01-26 20:05:44 -0300

    Fix date of birthay

--- sql/universidades-b.sql
+++ sql/universidades-b.sql
@@ -1,7 +1,7 @@
-SELECT universidad as university, carrera as career, name as full_name, sexo as gender, fecha_nacimiento as date_of_birth, codigo_postal as postal_code, 'null' as location, correo_electronico as email
+SELECT universidad as university, carrera as career, name as full_name, sexo as gender, TO_DATE(fecha_nacimiento,'YYYY-MM-DD') as date_of_birth, codigo_postal as postal_code, 'null' as location, correo_electronico as email
 FROM flores_comahue
 WHERE (universidad = 'UNIV. NACIONAL DEL COMAHUE') AND (TO_DATE(fecha_de_inscripcion,'YYYY-MM-DD') BETWEEN '2020/09/01' AND '2021/02/01')
 UNION
-SELECT universidad as university, carrera as career, nombre as full_name, sexo as gender, fecha_nacimiento as date_of_birth, 'null' as postal_code, localidad as location, email as email
+SELECT universidad as university, carrera as career, nombre as full_name, sexo as gender, TO_DATE(fecha_nacimiento,'DD/Mon/YYYY') as date_of_birth, 'null' as postal_code, localidad as location, email as email
 FROM salvador_villa_maria
-WHERE (universidad = 'UNIVERSIDAD_DEL_SALVADOR') AND (TO_DATE(fecha_de_inscripcion,'DD-Mon-YY') BETWEEN '2020/09/01' AND '2021/02/01')
+WHERE (universidad = 'UNIVERSIDAD_DEL_SALVADOR') AND (TO_DATE(fecha_de_inscripcion,'DD-Mon-YYYY') BETWEEN '2020/09/01' AND '2021/02/01')
