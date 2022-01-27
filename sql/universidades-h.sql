b46e4c93bcc649741c58dccd8473c08ac84feb61
Hectornauta <hemilianop13@ca.frre.utn.edu.ar>
2022-01-27 16:46:35 -0300

    postal_code and location are different columns

--- sql/universidades-h.sql
+++ sql/universidades-h.sql
@@ -1,10 +1,15 @@
-SELECT universities AS university,careers AS career,names AS full_name,sexo AS sex,birth_dates,locations as location_postal_code,emails
+SELECT universities AS university,careers AS career,names AS full_name,sexo AS sex,
+TO_DATE(birth_dates,'DD-MM-YYYY') AS birth_date,
+NULL as postal_code,locations as location,emails
 FROM lat_sociales_cine
-WHERE universities='UNIVERSIDAD-DEL-CINE'
+WHERE universities = 'UNIVERSIDAD-DEL-CINE'
 AND TO_DATE(inscription_dates,'DD-MM-YYYY') BETWEEN '2020-09-01' AND '2021-02-01'
 UNION ALL
-SELECT universidades,carreras,nombres,sexo,fechas_nacimiento,codigos_postales,emails
+SELECT universidades,carreras,nombres,sexo,
+TO_DATE(fechas_nacimiento,'YY-Mon-DD') AS birth_date,
+codigos_postales as postal_code,NULL as location,
+emails
 FROM uba_kenedy
-WHERE universidades='universidad-de-buenos-aires'
-AND TO_DATE(fechas_de_inscripcion,'YY-Mon-DD') BETWEEN '2020-09-01' AND '2021-02-01'
-ORDER BY university
+WHERE universidades = 'universidad-de-buenos-aires'
+AND TO_DATE(fechas_de_inscripcion,'YY-Mon-DD')
+BETWEEN '2020-09-01' AND '2021-02-01'
