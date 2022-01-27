507e5996f7696c606f9977a3179e426c8fc0be1b
aguyanzon <aguyanzon@hotmail.com>
2022-01-27 11:18:57 -0300

    add TO_DATE in SELECT statement

--- sql/universidades-c.sql
+++ sql/universidades-c.sql
@@ -1,9 +1,9 @@
-SELECT university, career, nombre AS full_name, sexo AS gender, birth_date, NULL AS postal_code, location, email 
+SELECT university, career, nombre AS full_name, sexo AS gender, TO_DATE(birth_date, 'YYYY/MM/DD') AS birth_date, NULL AS postal_code, location, email 
 FROM jujuy_utn
 WHERE (university = 'universidad nacional de jujuy')
 AND (inscription_date BETWEEN '2020/09/01' AND '2021/02/01')
 UNION
-SELECT universidad AS university, careers AS career, names AS full_name, sexo AS gender, birth_dates AS birth_date, codigo_postal AS postal_code, NULL AS location, correos_electronicos AS email 
+SELECT universidad AS university, careers AS career, names AS full_name, sexo AS gender, TO_DATE(birth_dates, 'DD/Mon/YY') AS birth_date, codigo_postal AS postal_code, NULL AS location, correos_electronicos AS email 
 FROM palermo_tres_de_febrero 
-WHERE (university = '_universidad_de_palermo')
+WHERE (universidad = '_universidad_de_palermo')
 AND (TO_DATE(fecha_de_inscripcion, 'DD/Mon/YY') BETWEEN '2020/09/01' AND '2021/02/01');
