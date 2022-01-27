64277c64a90b9145d1ed192854f80e922907ba65
Laureano Engulian <laureanoengulian@gmail.com>
2022-01-26 21:56:31 -0300

    sql query update

--- sql/.sql
+++ sql/.sql
@@ -1,10 +1,10 @@
-SELECT universidad as university, carrerra AS  career, nombrre AS name, sexo AS gender,
-nacimiento AS age, codgoposstal AS postal_code, direccion AS location, eemail AS email, 
+SELECT universidad AS university, carrerra AS  career, nombrre AS "name", sexo AS gender,
+nacimiento AS age, codgoposstal AS postal_code, 'null' AS "location", eemail AS email, 
 to_date(fechaiscripccion,'DD/MM/YYYY') AS inscription_date 
 FROM moron_nacional_pampa WHERE universidad = 'Universidad de morón' 
 AND to_date(fechaiscripccion,'DD/MM/YYYY') BETWEEN '2020-09-01' AND '2021-02-01'
 UNION 
-SELECT univiersities as university, carrera as career, names as name, sexo as gender, fechas_nacimiento as age,
-'null' as postal_code, localidad as location, email, to_date(inscription_dates,'DD/Mon/YY') as inscription_date 
-from rio_cuarto_interamericana WHERE univiersities = 'Universidad-nacional-de-río-cuarto' 
+SELECT univiersities AS university, carrera AS career, "names" AS "name", sexo AS gender, fechas_nacimiento AS age,
+'null' AS postal_code, localidad AS "location", email, to_date(inscription_dates,'DD/Mon/YY') AS inscription_date 
+FROM rio_cuarto_interamericana WHERE univiersities = 'Universidad-nacional-de-río-cuarto' 
 AND to_date(inscription_dates,'DD/Mon/YY') BETWEEN '2020-09-01' AND '2021-02-01'
