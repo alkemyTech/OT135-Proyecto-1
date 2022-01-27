929a53b1726e1158e4cea073df5e49be326ef4e1
Laureano Engulian <laureanoengulian@gmail.com>
2022-01-27 17:02:12 -0300

    dag update

---
+++ dag-universities-e.py
@@ -0,0 +1,15 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+
+with DAG(
+    'universidades-E',
+    description = 'Configuración un DAG, sin consultas, ni procesamiento para el grupo de universidades E',
+    schedule_interval = timedelta(hours = 1),
+    start_date = datetime(2022,1,27)
+) as dag:
+    extract = DummyOperator(task_id='extract from sql')
+    transform = DummyOperator(task_id='transform with pandas')
+    load = DummyOperator(task_id='load to s3')
+
+    extract >> transform >> load
