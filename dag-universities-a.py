8e8c9fab5ee4c7f1daac2beeacc1cecdccb71341
Juan Fernandez <juanfernandez_carc@hotmail.com>
2022-01-27 23:32:22 -0300

    set hour interval

--- dag-universities-a.py
+++ dag-universities-a.py
@@ -5,7 +5,7 @@ from airflow.operators.dummy import DummyOperator
 with DAG(
     'dag_universities_a',
     description='DAG para la Universidad De Flores y Universidad Nacional De Villa María. Documenta los operators que se utilizan a futuro, teniendo en cuenta que se va a hacer una consulta SQL, se van a procesar los datos con pandas y se van a cargar los datos en S3',
-    schedule_interval=timedelta(days=1),
+    schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 1, 26),
 ) as dag:
     query_sql = DummyOperator(task_id='query_sql')
