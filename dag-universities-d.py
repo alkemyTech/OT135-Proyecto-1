f99200d256c988dee7616a881877d23461c94f9f
nvrancovich <nvrancovich@gmail.com>
2022-01-28 08:19:20 -0300

    dagupdate

---
+++ dag-universities-d.py
@@ -0,0 +1,15 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+
+with DAG(
+    'dag-universities-d',
+    description='Configuración de un DAG para el grupo de universidades d',
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2022,1,27)
+) as dag:
+    extract = DummyOperator(task_id='extract from sql')
+    process = DummyOperator(task_id='process with pandas')
+    load = DummyOperator(task_id='load into database')
+
+    extract >> process >> load
