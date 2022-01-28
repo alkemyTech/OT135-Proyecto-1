e45b3bfc59a9fd366c1904d60faf80465b62bf3d
aguyanzon <aguyanzon@hotmail.com>
2022-01-28 18:29:03 -0300

    include CASE statement in birthday to filter dates

--- sql/universidades-c.sql
+++ sql/universidades-c.sql
@@ -1,9 +1,19 @@
-SELECT university, career, nombre AS full_name, sexo AS gender, TO_DATE(birth_date, 'YYYY/MM/DD') AS birth_date, NULL AS postal_code, location, email 
+SELECT university, career, nombre AS full_name, sexo AS gender,
+CASE 
+ WHEN TO_DATE(birth_date, 'YYYY/MM/DD') >= CURRENT_DATE - INTERVAL '16 years' THEN (TO_DATE(birth_date, 'YYYY/MM/DD') - INTERVAL '100 years')::date
+ ELSE TO_DATE(birth_date, 'YYYY/MM/DD')::date
+END AS birth_date,
+NULL AS postal_code, location, email 
 FROM jujuy_utn
 WHERE (university = 'universidad nacional de jujuy')
 AND (inscription_date BETWEEN '2020/09/01' AND '2021/02/01')
 UNION
-SELECT universidad AS university, careers AS career, names AS full_name, sexo AS gender, TO_DATE(birth_dates, 'DD/Mon/YY') AS birth_date, codigo_postal AS postal_code, NULL AS location, correos_electronicos AS email 
+SELECT universidad AS university, careers AS career, names AS full_name, sexo AS gender, 
+CASE 
+ WHEN TO_DATE(birth_dates, 'DD/Mon/YY') >= CURRENT_DATE - INTERVAL '16 years' THEN (TO_DATE(birth_dates, 'DD/Mon/YY') - INTERVAL '100 years')::date
+ ELSE TO_DATE(birth_dates, 'DD/Mon/YY')::date
+END AS birth_date,
+codigo_postal AS postal_code, NULL AS location, correos_electronicos AS email 
 FROM palermo_tres_de_febrero 
 WHERE (universidad = '_universidad_de_palermo')
 AND (TO_DATE(fecha_de_inscripcion, 'DD/Mon/YY') BETWEEN '2020/09/01' AND '2021/02/01');
