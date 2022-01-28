f70828d98d18233ffb9cdc7811be3f4b3939aa5a
Laureano Engulian <laureanoengulian@gmail.com>
2022-01-28 18:04:22 -0300

    code fixed

--- dag-universities-e.py
+++ dag-universities-e.py
@@ -8,8 +8,8 @@ with DAG(
     schedule_interval = timedelta(hours = 1),
     start_date = datetime(2022,1,27)
 ) as dag:
-    extract = DummyOperator(task_id='extract from sql')
-    transform = DummyOperator(task_id='transform with pandas')
-    load = DummyOperator(task_id='load to s3')
+    extract_from_sql = DummyOperator(task_id='extract_from_sql')
+    transform_with_pandas = DummyOperator(task_id='transform_with_pandas')
+    load_to_s3 = DummyOperator(task_id='load_to_s3')
 
     extract >> transform >> load
