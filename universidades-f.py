354e31ef720bf543bbb55e71f9c836173259acfe
Hector Emiliano Pollero <hemilianop13@gmail.com>
2022-01-28 23:43:38 -0300

    Use of pep8 and fixed task_ids with errors

--- universidades-f.py
+++ universidades-f.py
@@ -1,16 +1,15 @@
 from datetime import timedelta, datetime
-
 from airflow import DAG
-
 from airflow.operators.dummy import DummyOperator
 
 with DAG(
     'universidades-F',
     description = 'DAG correspondiente a las universidades de Morón y Río Cuarto',
-    schedule_interval = timedelta(hours = 1),
-    start_date = datetime(2022,1,28)
+    schedule_interval = timedelta(hours=1),
+    start_date = datetime(2022, 1, 28)
 ) as dag:
-    extract = DummyOperator(task_id = 'Extract data from DB')
-    transform = DummyOperator(task_id = 'Transform data using Pandas')
-    load = DummyOperator(task_id = 'Load data')
+    extract = DummyOperator(task_id = 'extract_data')
+    transform = DummyOperator(task_id = 'transform_data')
+    load = DummyOperator(task_id = 'load_data')
+
     extract >> transform >> load
