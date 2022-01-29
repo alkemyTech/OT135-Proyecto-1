d0ad670d1ea0bd801851526a3546c9c8aeb6fdab
Hector Emiliano Pollero <hemilianop13@gmail.com>
2022-01-28 22:46:00 -0300

    Use of SQL style Guide

--- sql/universidades-h.sql
+++ sql/universidades-h.sql
@@ -1,21 +1,33 @@
-SELECT universities AS university,careers AS career,names AS full_name,sexo AS sex,
-CASE
- WHEN TO_DATE(birth_dates,'DD-MM-YYYY') >= CURRENT_DATE - INTERVAL '16 years' THEN (TO_DATE(birth_dates,'DD-MM-YYYY') - INTERVAL '100 years')::date
- ELSE TO_DATE(birth_dates,'DD-MM-YYYY')::date
-END AS birth_date,
-NULL as postal_code,locations as location,emails
+SELECT
+  universities     AS university,
+  careers          AS career,
+  names            AS student_full_name,
+  sexo             AS gender,
+  CASE 
+    WHEN TO_DATE(birth_dates,'DD-MM-YYYY') >= CURRENT_DATE - INTERVAL '16 years'
+      THEN (TO_DATE(birth_dates,'DD-MM-YYYY') - INTERVAL '100 years')::date
+    ELSE TO_DATE(birth_dates,'DD-MM-YYYY')::date
+  END             AS birth_date,
+  NULL            AS postal_code,
+  locations       AS location,
+  emails          AS email
 FROM lat_sociales_cine
 WHERE universities = 'UNIVERSIDAD-DEL-CINE'
-AND TO_DATE(inscription_dates,'DD-MM-YYYY') BETWEEN '2020-09-01' AND '2021-02-01'
+  AND TO_DATE(inscription_dates,'DD-MM-YYYY') BETWEEN '2020-09-01' AND '2021-02-01'
 UNION ALL
-SELECT universidades,carreras,nombres,sexo,
-CASE
- WHEN TO_DATE(fechas_nacimiento,'YY-Mon-DD') >= CURRENT_DATE - INTERVAL '16 years' THEN (TO_DATE(fechas_nacimiento,'YY-Mon-DD') - INTERVAL '100 years')::date
- ELSE TO_DATE(fechas_nacimiento,'YY-Mon-DD')::date
-END AS birth_date,
-codigos_postales as postal_code,NULL as location,
-emails
+SELECT
+  universidades    AS university,
+  carreras         AS career,
+  nombres          AS student_full_name,
+  sexo             AS gender,
+  CASE
+   WHEN TO_DATE(fechas_nacimiento,'YY-Mon-DD') >= CURRENT_DATE - INTERVAL '16 years'
+     THEN (TO_DATE(fechas_nacimiento,'YY-Mon-DD') - INTERVAL '100 years')::date
+   ELSE TO_DATE(fechas_nacimiento,'YY-Mon-DD')::date
+  END              AS birth_date,
+  codigos_postales AS postal_code,
+  NULL             AS location,
+  emails           AS email
 FROM uba_kenedy
 WHERE universidades = 'universidad-de-buenos-aires'
-AND TO_DATE(fechas_de_inscripcion,'YY-Mon-DD')
-BETWEEN '2020-09-01' AND '2021-02-01'
+  AND TO_DATE(fechas_de_inscripcion,'YY-Mon-DD') BETWEEN '2020-09-01' AND '2021-02-01'
