f2eaac0e1e5c65a0dd63d336d7aa3decd330eaf9
aguyanzon <aguyanzon@hotmail.com>
2022-01-30 17:09:14 -0300

    remove spaces in dag parameters

--- dag-universities-b.py
+++ dag-universities-b.py
@@ -4,9 +4,9 @@ from airflow.operators.dummy import DummyOperator
 
 with DAG(
     'dag-universities-b',
-    description= 'Hacer un ETL para la Universidad Nacional del Comahue y Universidad del Salvador',
-    schedule_interval= '@hourly',
-    start_date= datetime(2022, 1, 26)
+    description='Hacer un ETL para la Universidad Nacional del Comahue y Universidad del Salvador',
+    schedule_interval='@hourly',
+    start_date=datetime(2022, 1, 26)
 ) as dag:
     sql_query = DummyOperator(task_id='sql_query')
     pandas_processing = DummyOperator(task_id='pandas_processing')
