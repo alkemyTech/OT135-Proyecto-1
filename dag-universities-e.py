3da5ee1a2937ad87d737fda6ab314dbb5f799351
Laureano Engulian <laureanoengulian@gmail.com>
2022-01-30 18:10:50 -0300

    code fixed

--- dag-universities-e.py
+++ dag-universities-e.py
@@ -4,12 +4,12 @@ from airflow.operators.dummy import DummyOperator
 
 with DAG(
     'dag-universities-e',
-    description = 'Configuración un DAG, sin consultas, ni procesamiento para el grupo de universidades E',
-    schedule_interval = timedelta(hours = 1),
-    start_date = datetime(2022,1,27)
+    description='Configuración un DAG, sin consultas, ni procesamiento para el grupo de universidades E',
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2022, 1, 27)
 ) as dag:
     extract_from_sql = DummyOperator(task_id='extract_from_sql')
     transform_with_pandas = DummyOperator(task_id='transform_with_pandas')
     load_to_s3 = DummyOperator(task_id='load_to_s3')
 
-    extract >> transform >> load
+    extract_from_sql >> transform_with_pandas >> load_to_s3
