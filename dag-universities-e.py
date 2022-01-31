b53365e0f37d7faa54d26a6db67b16f58ea1ab0c
Luis David Tudares <ldavidts@gmail.com>
2022-01-31 19:29:11 -0300

    created logger and logs on console as requested

--- dag-universities-e.py
+++ dag-universities-e.py
@@ -1,6 +1,22 @@
+import logging
+
 from datetime import timedelta, datetime
+
 from airflow import DAG
+
 from airflow.operators.dummy import DummyOperator
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s: %(message)s: %(levelname)s: ","%Y-%m-%d")
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter) 
+
+logger.addHandler(stream_handler)
+
 
 with DAG(
     'dag-universities-e',
@@ -8,7 +24,14 @@ with DAG(
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 1, 27)
 ) as dag:
-    extract_from_sql = DummyOperator(task_id='extract_from_sql')
+    try:
+        extract_from_sql = DummyOperator(task_id='extract_from_sql')
+        logger.info(" Universidad Nacional De La Pampa ")
+        logger.info(" Universidad Abierta Interamericana ")
+    except:
+        logger.error(" Universidad Nacional De La Pampa ")
+        logger.error(" Universidad Abierta Interamericana ")
+
     transform_with_pandas = DummyOperator(task_id='transform_with_pandas')
     load_to_s3 = DummyOperator(task_id='load_to_s3')
 
