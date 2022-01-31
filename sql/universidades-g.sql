4b5bdc155d0dfd2b9491806e44497bdf47f6457d
Luis David Tudares <ldavidts@gmail.com>
2022-01-31 12:17:21 -0300

    modified code to fit 'sql style guide'

--- sql/universidades-g.sql
+++ sql/universidades-g.sql
@@ -1,14 +1,30 @@
-SELECT universities AS universidades, careers AS carreras, names AS nombres, sexo, TO_DATE(birth_dates,'DD/MM/YYYY') AS fechas_nacimiento, null AS codigos_postales, locations AS direcciones, emails, TO_DATE(inscription_dates,'DD/MM/YYYY') AS fechas_de_inscripcion
+SELECT 
+  universities AS university, 
+  careers AS career, 
+  names AS full_name, 
+  sexo AS gender, 
+  TO_DATE(birth_dates,'DD/MM/YYYY') AS birth_date, 
+  null AS zipcode, 
+  locations AS adress, 
+  emails AS email, 
+  TO_DATE(inscription_dates,'DD/MM/YYYY') AS inscription_date
 FROM lat_sociales_cine
 WHERE universities = '-FACULTAD-LATINOAMERICANA-DE-CIENCIAS-SOCIALES'
-AND inscription_dates BETWEEN '01-09-2020' AND '02-01-2021'
+  AND inscription_dates BETWEEN '01-09-2020' AND '02-01-2021'
 UNION
-SELECT universidades, carreras, nombres, sexo, 
-CASE
- WHEN TO_DATE(fechas_nacimiento,'YY-Mon-DD') >= CURRENT_DATE - INTERVAL '16 years' THEN TO_DATE(fechas_nacimiento,'YY-Mon-DD') - INTERVAL '100 years'
- ELSE TO_DATE(fechas_nacimiento,'YY-Mon-DD')
-END AS fechas_nacimiento,
-codigos_postales, direcciones, emails, TO_DATE(fechas_de_inscripcion,'YY/Mon/DD')
+SELECT 
+  universidades AS university, 
+  carreras AS career, 
+  nombres AS full_name, 
+  sexo AS gender, 
+  CASE
+     WHEN TO_DATE(fechas_nacimiento,'YY-Mon-DD') >= CURRENT_DATE - INTERVAL '16 years' THEN TO_DATE(fechas_nacimiento,'YY-Mon-DD') - INTERVAL '100 years'
+     ELSE TO_DATE(fechas_nacimiento,'YY-Mon-DD')
+  END AS birth_date,
+  codigos_postales AS zipcode, 
+  direcciones AS adress,
+  emails AS email, 
+  TO_DATE(fechas_de_inscripcion,'YY/Mon/DD') AS inscription_date
 FROM uba_kenedy
 WHERE universidades ='universidad-j.-f.-kennedy'
-AND fechas_de_inscripcion BETWEEN '20-Sep-09' AND '21-Ene-02';
+  AND fechas_de_inscripcion BETWEEN '20-Sep-09' AND '21-Ene-02';
