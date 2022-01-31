32a934ad3a6743d6b8343cc3375226c52b05ad92
Hector Emiliano Pollero <hemilianop13@gmail.com>
2022-01-31 14:04:01 -0300

    Added retries to dag and some pep8 fixes

--- dag-universities-e.py
+++ dag-universities-e.py
@@ -4,9 +4,13 @@ from airflow.operators.dummy import DummyOperator
 
 with DAG(
     'dag-universities-e',
-    description='Configuración un DAG, sin consultas, ni procesamiento para el grupo de universidades E',
-    schedule_interval=timedelta(hours=1),
-    start_date=datetime(2022, 1, 27)
+    description = 'Configuración un DAG, sin consultas, ni procesamiento para el grupo de universidades E',
+    default_args = {
+        'retries': 5,
+        'retry_delay': timedelta(minutes=5)
+        },
+    schedule_interval = timedelta(hours=1),
+    start_date = datetime(2022, 1, 27)
 ) as dag:
     extract_from_sql = DummyOperator(task_id='extract_from_sql')
     transform_with_pandas = DummyOperator(task_id='transform_with_pandas')
