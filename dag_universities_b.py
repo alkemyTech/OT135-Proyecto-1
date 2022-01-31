aeb4dc7f54982eab54fc19b1c77cbf6ed130c522
maxinbury <maxinbury@gmail.com>
2022-01-31 16:51:56 -0300

    universities were corrected

--- dag_universities_b.py
+++ dag_universities_b.py
@@ -1,14 +1,11 @@
 from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-import logging as log
 
-log.basicConfig(filename='app.log', level=log.ERROR, 
-    format='%(asctime)s - %(processName)s - %(message)s', datefmt='%Y-%m-%d')
 
 with DAG(
-    'dag_universities_c',
-    description='DAG  Univ. Nacional De Jujuy - Universidad De Palermo',
+    'dag_universities_b',
+    description='DAG  Univ. Nacional Del Comahue - Universidad Del Salvador',
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 1, 26),
     retries=10,
