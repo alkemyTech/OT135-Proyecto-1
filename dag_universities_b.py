d0f4c18b47e3ad8f4d31f89c72f0b6891d20f946
maxinbury <maxinbury@gmail.com>
2022-01-31 14:31:53 -0300

    the retries was added

---
+++ dag_universities_b.py
@@ -0,0 +1,22 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+import logging as log
+
+log.basicConfig(filename='app.log', level=log.ERROR, 
+    format='%(asctime)s - %(processName)s - %(message)s', datefmt='%Y-%m-%d')
+
+with DAG(
+    'dag_universities_a',
+    description='DAG  Univ. Nacional Del Comahue - Universidad Del Salvador',
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2022, 1, 26),
+    retries=10,
+    retry_delay=timedelta(seconds=1)
+    
+) as dag:
+    query_sql = DummyOperator(task_id='query_sql')
+    pandas_process = DummyOperator(task_id='pandas_process')
+    load_S3 = DummyOperator(task_id='load_S3')
+
+    query_sql >> pandas_process >> load_S3
