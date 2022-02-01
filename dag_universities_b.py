17a1a89ba11e5d3bca06d50e463ec244fd9cb718
maxinbury <maxinbury@gmail.com>
2022-02-01 17:47:40 -0300

    se modifica el archivo del main dag_universities_b.py

--- dag_universities_b.py
+++ dag_universities_b.py
@@ -1,18 +1,14 @@
-import logging as log
-
 from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-
-# Standard  logging config.
-log.basicConfig(level=log.ERROR, 
-    format='%(asctime)s - %(processName)s - %(message)s', datefmt='%Y-%m-%d')
 
 with DAG(
     'dag_universities_b',
     description='DAG  Univ. Nacional Del Comahue - Universidad Del Salvador',
     schedule_interval=timedelta(hours=1),
-    start_date=datetime(2022, 1, 26)
+    start_date=datetime(2022, 1, 26),
+    retries = 10,
+    retry_delay=timedelta(seconds=300)
     
 ) as dag:
     query_sql = DummyOperator(task_id='query_sql')
