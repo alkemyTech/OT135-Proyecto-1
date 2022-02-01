bfac831f68f96f529f32deaef58e0c1d0414c3ff
Luis David Tudares <ldavidts@gmail.com>
2022-02-01 19:06:31 -0300

    modified time formatting and deleted extra information

--- dag-universities-e.py
+++ dag-universities-e.py
@@ -24,14 +24,8 @@ with DAG(
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 1, 27)
 ) as dag:
-    try:
-        extract_from_sql = DummyOperator(task_id='extract_from_sql')
-        logger.info(" Universidad Nacional De La Pampa ")
-        logger.info(" Universidad Abierta Interamericana ")
-    except:
-        logger.error(" Universidad Nacional De La Pampa ")
-        logger.error(" Universidad Abierta Interamericana ")
-
+    
+    extract_from_sql = DummyOperator(task_id='extract_from_sql')
     transform_with_pandas = DummyOperator(task_id='transform_with_pandas')
     load_to_s3 = DummyOperator(task_id='load_to_s3')
 
