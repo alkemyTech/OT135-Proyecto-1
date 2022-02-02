b3d06ffbd86653ba0aaaeaa520afc3e0098b1f8f
Virginia Ambrosino <virginiaamb@gmail.com>
2022-02-02 16:28:30 -0300

    implemented python operator

--- dag-universities-e.py
+++ dag-universities-e.py
@@ -1,35 +1,68 @@
+import pandas as pd
 import logging
+import csv
+from sqlalchemy import create_engine, text
 
 from datetime import timedelta, datetime
-
 from airflow import DAG
-
 from airflow.operators.dummy import DummyOperator
-
+from airflow.operators.python import PythonOperator
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter("%(asctime)s: %(module)s: %(message)s","%Y-%m-%d")
+formatter = logging.Formatter("%(asctime)s: %(module)s: %(message)s", "%Y-%m-%d")
 
 stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter) 
+stream_handler.setFormatter(formatter)
 
 logger.addHandler(stream_handler)
+
+# ideal crear un archivo .env
+DB_USER = "alkemy_super_user"
+DB_PASSWORD = "JkG3Ymc3AZuu"
+DB_HOST = "training-main.cghe7e6sfljt.us-east-1.rds.amazonaws.com"
+DB_PORT = 5432
+DB_DB = "training"
+
+SQL_SCRIPT = "sql/universidades-e.sql"
+
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DB}".format(), echo=False, client_encoding='utf8')
+
+
+def extract():
+    '''Toma la conexion a la base de datos engine y a partir del sql.script
+    definido ejecuta sus queries sobre la base de datos y lo guarda como un
+    archivo csv'''
+    try:
+        query = open(SQL_SCRIPT, 'r')
+        df_query = pd.read_sql_query(query.read(), engine)
+        query.close()
+        logger.info('sql successfully executed')
+        df_query.to_csv('files/universities-e.csv')
+        logger.info('csv successfully save')
+    except Exception as e:
+        logger.error(e)
 
 
 with DAG(
     'dag-universities-e',
-    description = 'Configuración un DAG, sin consultas, ni procesamiento para el grupo de universidades E',
-    default_args = {
+    description='Configuración de DAG para el grupo de universidades E',
+    default_args={
         'retries': 5,
         'retry_delay': timedelta(minutes=5)
         },
-    schedule_interval = timedelta(hours=1),
-    start_date = datetime(2022, 1, 27)
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2022, 1, 27)
 ) as dag:
-    
-    extract_from_sql = DummyOperator(task_id='extract_from_sql')
+    extract_from_sql = PythonOperator(
+        task_id="extract_from_sql",
+        python_callable=extract,
+        # op_kwargs: Optional[Dict] = None,
+        # op_args: Optional[List] = None,
+        # templates_dict: Optional[Dict] = None
+        # templates_exts: Optional[List] = None
+    )
     transform_with_pandas = DummyOperator(task_id='transform_with_pandas')
     load_to_s3 = DummyOperator(task_id='load_to_s3')
 
