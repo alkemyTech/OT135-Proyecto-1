c0718aced32acd88a111fa6ee563fef827ed0de3
Luis David Tudares <ldavidts@gmail.com>
2022-02-03 00:10:07 -0300

    added python operator and callable function

--- dag-universities-d.py
+++ dag-universities-d.py
@@ -1,8 +1,24 @@
+from lib2to3.pgen2.pgen import DFAState
 import logging
-
+import os
 from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+import pandas as pd
+
+def extract_process():
+    user = 'alkemy_super_user'
+    passwd = 'JkG3Ymc3AZuu'
+    host = 'training-main.cghe7e6sfljt.us-east-1.rds.amazonaws.com'
+    port = '5432'
+    db = 'training'
+    sql = open('sql/universidades-d.sql', 'r').read()
+    df = pd.read_sql(f"{sql}", f'postgresql://{user}:{passwd}@{host}:{port}/{db}')
+    if os.path.exists("/home/ldavidts/apache-airflow-aceleracion/airflow/OT135-Proyecto-1/file") == False:
+        os.makedirs("/home/ldavidts/apache-airflow-aceleracion/airflow/OT135-Proyecto-1/file")
+    return df.to_csv("/home/ldavidts/apache-airflow-aceleracion/airflow/OT135-Proyecto-1/file/universidades-d.csv")
+
 
 
 #UNIVERSIDADES = 'Universidad Tecnológica Nacional / Universidad Nacional De Tres De Febrero'
@@ -26,7 +42,10 @@ with DAG(
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022,1,27)
 ) as dag:
-    extract = DummyOperator(task_id='extract')
+    extract = PythonOperator(
+        task_id='extract',
+        python_callable=extract_process()
+    )
     process = DummyOperator(task_id='process')
     load = DummyOperator(task_id='load')
 
