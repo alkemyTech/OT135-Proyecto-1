a42349cd7dae456290da1fc760456f0bd59dddac
Hector Emiliano Pollero <hemilianop13@gmail.com>
2022-02-03 17:37:03 -0300

    Added Python Operator (SQL)"

--- dag_universities_c.py
+++ dag_universities_c.py
@@ -1,11 +1,36 @@
 import logging
-
+from os import makedirs
 from datetime import timedelta, datetime
+
+import pandas as pd
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 
-logging.basicConfig(level=logging.ERROR,
-                    format='%(asctime)s - %(module)s - %(message)s', datefmt='%Y-%m-%d')
+import config
+
+logging.basicConfig(level=logging.DEBUG, #previously ERROR
+                    format='%(asctime)s - %(module)s - %(message)s',
+                    datefmt='%Y-%m-%d'
+                    )
+
+def extract_data():
+    PATH_TO_CSV_FILES = 'airflow/dags/files'
+    ARCHIVO = 'airflow/dags/sql/universidades-c.sql'
+    makedirs(PATH_TO_CSV_FILES, exist_ok=True)
+    sql_connection = ('postgresql+psycopg2://'
+        + config.DB_USER
+        + ':'
+        + config.DB_PASSWORD
+        + '@'
+        + config.DB_HOST
+        + '/'
+        + config.DB_NAME
+    )
+    with open(ARCHIVO,'r') as sql_file:
+        sql_query = sql_file.read()
+    dataframe = pd.read_sql_query(sql_query,sql_connection)
+    dataframe.to_csv(PATH_TO_CSV_FILES + '/universities_c.csv')
 
 default_args = {
     'retries': 1,
@@ -19,7 +44,10 @@ with DAG(
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 1, 26)
 ) as dag:
-    query_sql = DummyOperator(task_id='query_sql')
+    query_sql = PythonOperator(
+        task_id = 'query_sql',
+        python_callable = extract_data
+        )
     pandas_process = DummyOperator(task_id='pandas_process')
     load_S3 = DummyOperator(task_id='load_S3')
 
