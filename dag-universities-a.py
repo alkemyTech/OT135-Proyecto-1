b85829185ec16528a10ee0ef1ce717b4c181c2f0
nvrancovich <nvrancovich@gmail.com>
2022-02-04 10:27:38 -0300

    adding python operator for sql query

--- dag-universities-a.py
+++ dag-universities-a.py
@@ -1,11 +1,38 @@
+import os
 import logging
 from datetime import timedelta, datetime
+from shutil import ExecError
+from decouple import config
+import pandas as pd
+from sqlalchemy import create_engine
+from signal import pthread_kill
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 
 logging.basicConfig(level=logging.ERROR,
                     format='%(asctime)s - %(module)s - %(message)s',
                     datefmt='%Y-%m-%d')
+
+DIR = os.path.dirname(__file__)
+DB_USER = 'alkemy_super_user'
+DB_PASSWORD = 'JkG3Ymc3AZuu'
+DB_HOST = 'training-main.cghe7e6sfljt.us-east-1.rds.amazonaws.com'
+DB_NAME = 'training'
+DB_PORT = 5432
+QUERY = f"{DIR}/sql/universidades-e.sql"
+
+def query():
+    try:
+        engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}".format(),
+                               echo=False, 
+                               client_encoding='utf8')
+        query = open(QUERY, 'r')
+        df_query = pd.read_sql_query(query.read(), engine)
+        df_query.to_csv(f'{DIR}/files/universities-e.csv')
+    except:
+        logging.error(Exception) # Si ocurre un error no muestra el nombre del grupo de universidades
+
 
 default_args = {
     'retries': 1,
@@ -20,7 +47,8 @@ with DAG(
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 1, 26),
 ) as dag:
-    query_sql = DummyOperator(task_id='query_sql') # Consulta SQL
+    query_sql = PythonOperator(task_id='query_sql',
+                               python_callable=query) # Consulta SQL
     pandas_process = DummyOperator(task_id='pandas_process') # Procesar datos con pandas
     load_S3 = DummyOperator(task_id='load_S3') # Carga de datos en S3
 
