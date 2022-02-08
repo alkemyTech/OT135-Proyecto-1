9d3e949b8889d026dd7325ed24cb3b2ffe564c50
Luis David Tudares <ldavidts@gmail.com>
2022-02-08 11:11:32 -0300

    reordered elements inside of .py, added logger, deleted unnecessary files

--- dag_universities_c.py
+++ dag_universities_c.py
@@ -7,6 +7,10 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 import pandas as pd
 
+logging.basicConfig(level=logging.ERROR,
+                    format='%(asctime)s - %(module)s - %(message)s', datefmt='%Y-%m-%d')
+logger = logging.getLogger(__name__)
+
 def csv_processing():
     """
     A function to be used as a pyhton operator to read the csv file and process later with pandas
@@ -14,12 +18,10 @@ def csv_processing():
     dir = os.path.dirname(__file__)
     try: 
         df = pd.read_csv(f"{dir}/files/universidades-c.csv")
-        logging.info(df.head())
-    except Exception as e:
-        logging.error(e)
-        
-logging.basicConfig(level=logging.ERROR,
-                    format='%(asctime)s - %(module)s - %(message)s', datefmt='%Y-%m-%d')
+    except FileNotFoundError:
+        logger.error("the file you are trying to access doesn't exist, check the path and the name of the file"  )
+    else:
+        logger.info(df.head())    
 
 default_args = {
     'retries': 1,
