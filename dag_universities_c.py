173e204c83b18d8433ecd0720602182b09b57e68
Hectornauta <hemilianop13@ca.frre.utn.edu.ar>
2022-02-08 10:19:14 -0300

    Added Python Operator (SQL) (#34)
    * Added Python Operator (SQL)"
    
    * Use os.path.dirname
    
    * Small fix
    
    * Added error handling and .env
    
    * Added template.cfg
    
    * Applied f-strings, repositioning of logging and raise of exceptions
    
    * Deletion of template.cfg
    
    Co-authored-by: Hector Emiliano Pollero <hemilianop13@gmail.com>

--- dag_universities_c.py
+++ dag_universities_c.py
@@ -1,11 +1,59 @@
 import logging
-
+import os
 from datetime import timedelta, datetime
+
+import pandas as pd
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from decouple import config
+from sqlalchemy import exc
 
-logging.basicConfig(level=logging.ERROR,
-                    format='%(asctime)s - %(module)s - %(message)s', datefmt='%Y-%m-%d')
+logging.basicConfig(level=logging.DEBUG, #previously ERROR
+                    format='%(asctime)s - %(module)s - %(message)s',
+                    datefmt='%Y-%m-%d'
+                    )
+
+DB_USER = config('DB_USER') 
+DB_PASSWORD = config('DB_PASSWORD') 
+DB_HOST = config('DB_HOST') 
+DB_PORT = config('DB_PORT') 
+DB_NAME = config('DB_NAME') 
+
+def extract_data():
+    DIR = os.path.dirname(__file__)
+    PATH_TO_CSV_FILES =  f'{DIR}/files'
+    ARCHIVO = f'{DIR}/sql/universidades-c.sql'
+    try:
+        os.makedirs(PATH_TO_CSV_FILES, exist_ok=True)
+    except IOError as e:
+        logging.error(f'Error al crear los directorios: {e}')
+        raise Exception('Ha ocurrido un error al crear los directorios')
+    sql_connection = ('postgresql+psycopg2://'
+        + DB_USER
+        + ':'
+        + DB_PASSWORD
+        + '@'
+        + DB_HOST
+        + '/'
+        + DB_NAME
+    )
+    try:
+        with open(ARCHIVO,'r') as sql_file:
+            sql_query = sql_file.read()
+    except IOError as e:
+        logging.error(f'Error al leer los archivos SQL: {e}')
+        raise Exception('Ha ocurrido un error al leer los archivos SQL')
+    try:
+        dataframe = pd.read_sql_query(sql_query,sql_connection)
+    except exc.SQLAlchemyError as e:
+        logging.error(f'Error al trabajar con la base de datos: {e}')
+        raise Exception('Error al trabajar con la base de datos')
+    try:
+        dataframe.to_csv(f'{PATH_TO_CSV_FILES}/universities_c.csv', encoding='utf-8-sig', index=False)
+    except IOError as e:
+        logging.error(f'Error al crear los archivos CSV: {e}')
+        raise Exception('Ha ocurrido un error al crear los archivos CSV')
 
 default_args = {
     'retries': 1,
@@ -19,7 +67,10 @@ with DAG(
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 1, 26)
 ) as dag:
-    query_sql = DummyOperator(task_id='query_sql')
+    query_sql = PythonOperator(
+        task_id = 'query_sql',
+        python_callable = extract_data
+        )
     pandas_process = DummyOperator(task_id='pandas_process')
     load_S3 = DummyOperator(task_id='load_S3')
 
