911d1657c206d6ab4405fe319bd3b781d2fa864e
Hector Emiliano Pollero <hemilianop13@gmail.com>
2022-02-08 02:07:04 -0300

    Applied f-strings, repositioning of logging and raise of exceptions

--- dag_universities_c.py
+++ dag_universities_c.py
@@ -9,26 +9,27 @@ from airflow.operators.python import PythonOperator
 from decouple import config
 from sqlalchemy import exc
 
+logging.basicConfig(level=logging.DEBUG, #previously ERROR
+                    format='%(asctime)s - %(module)s - %(message)s',
+                    datefmt='%Y-%m-%d'
+                    )
+
 DB_USER = config('DB_USER') 
 DB_PASSWORD = config('DB_PASSWORD') 
 DB_HOST = config('DB_HOST') 
 DB_PORT = config('DB_PORT') 
 DB_NAME = config('DB_NAME') 
 
-logging.basicConfig(level=logging.DEBUG, #previously ERROR
-                    format='%(asctime)s - %(module)s - %(message)s',
-                    datefmt='%Y-%m-%d'
-                    )
 
 def extract_data():
     DIR = os.path.dirname(__file__)
-    PATH_TO_CSV_FILES = DIR + '/files'
-    ARCHIVO = DIR + '/sql/universidades-c.sql'
+    PATH_TO_CSV_FILES =  f'{DIR}/files'
+    ARCHIVO = f'{DIR}/sql/universidades-c.sql'
     try:
         os.makedirs(PATH_TO_CSV_FILES, exist_ok=True)
     except IOError as e:
-        logging.error('Error al crear los directorios: ' + str(e))
-        sys.exit('Ha ocurrido un error al crear los directorios')
+        logging.error(f'Error al crear los directorios: {e}')
+        raise Exception('Ha ocurrido un error al crear los directorios')
     sql_connection = ('postgresql+psycopg2://'
         + DB_USER
         + ':'
@@ -42,18 +43,18 @@ def extract_data():
         with open(ARCHIVO,'r') as sql_file:
             sql_query = sql_file.read()
     except IOError as e:
-        logging.error('Error al leer los archivos sql: ' + str(e))
-        sys.exit('Ha ocurrido un error al leer los archivos SQL')
+        logging.error(f'Error al leer los archivos SQL: {e}')
+        raise Exception('Ha ocurrido un error al leer los archivos SQL')
     try:
         dataframe = pd.read_sql_query(sql_query,sql_connection)
-    except exc.SQLAlchemyError:
-        logging.error('Error al conectar a la base de datos')
-        sys.exit('Error al conectar a la base de datos')
+    except exc.SQLAlchemyError as e:
+        logging.error(f'Error al trabajar con la base de datos: {e}')
+        raise Exception('Error al trabajar con la base de datos')
     try:
-        dataframe.to_csv(PATH_TO_CSV_FILES + '/universities_c.csv', encoding='utf-8-sig', index=False)
+        dataframe.to_csv(f'{PATH_TO_CSV_FILES}/universities_c.csv', encoding='utf-8-sig', index=False)
     except IOError as e:
-        logging.error('Error al crear los archivos csv: ' + str(e))
-        sys.exit('Ha ocurrido un error al crear los archivos csv')
+        logging.error(f'Error al crear los archivos CSV: {e}')
+        raise Exception('Ha ocurrido un error al crear los archivos CSV')
 
 default_args = {
     'retries': 1,
