81f3f6113f412d19dd3d5950f625ace095af1c3d
julianpalmerio <julianpalmerio@hotmail.com>
2022-02-08 11:21:49 -0300

    Change exist_ok in makedirs, add raise ex, and some constant names has changed.

--- dag-universities-d.py
+++ dag-universities-d.py
@@ -23,7 +23,7 @@ DB_USER = config('DB_USER')
 DB_PASSWORD = config('DB_PASSWORD')
 DB_HOST = config('DB_HOST')
 DB_PORT = config('DB_PORT')
-PSQL_DB = config('PSQL_DB')
+DB_NAME = config('DB_NAME')
 
 def extract_process():
     """
@@ -32,11 +32,12 @@ def extract_process():
     home = os.path.dirname(__file__)
     with open(f'{home}/sql/universidades-d.sql', 'r') as sql: 
         try:
-            df = pd.read_sql(sql.read(), f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{PSQL_DB}')
-        except FileNotFoundError:    
-            logger.error("sorry the file doesn't exist") 
+            df = pd.read_sql(sql.read(), f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+        except FileNotFoundError as ex:    
+            logger.error("sorry the file doesn't exist")
+	    raise ex
         else:
-            os.makedirs(f"{home}/files",mode = 0o777, exist_ok = False)
+            os.makedirs(f"{home}/files", exist_ok = True)
             df.to_csv(f"{home}/files/universidades-d.csv")
             logger.info('csv file created correctly')
 
