81314b24630aaa41c0f20313408649228a6600c0
Hectornauta <hemilianop13@ca.frre.utn.edu.ar>
2022-02-09 12:56:01 -0300

    Implementada la función para normalizar datos de las universidades A (#40)
    * Function implemented, testing required
    
    * Exception handling
    
    * Drop birth_date and fix for locations
    
    * Applied pep8 style, imported libraries and dag configured
    
    * DAG should not be configured
    
    * Split into two txt files
    
    Co-authored-by: Hector Emiliano Pollero <hemilianop13@gmail.com>
    Co-authored-by: julianpalmerio <julianpalmerio@hotmail.com>

--- dag-universities-a.py
+++ dag-universities-a.py
@@ -1,18 +1,21 @@
 import os
 import logging
-from datetime import timedelta, datetime
-from decouple import config
 import pandas as pd
+from datetime import timedelta, datetime, date
+from decouple import config
 from sqlalchemy import create_engine
 from signal import pthread_kill
+
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 
-logger = logging.getLogger()
+
 logging.basicConfig(level=logging.ERROR,
                     format='%(asctime)s - %(module)s - %(message)s',
                     datefmt='%Y-%m-%d')
+
+logger = logging.getLogger('DAG-A')
 
 def query():
     try:
@@ -33,6 +36,113 @@ def query():
     except Exception as e:
         logger.error('Hubo un error en la consulta sql en las tablas de la  Universidad De Flores y/o la Universidad Nacional De Villa María')
         raise e
+
+def age(birth_date):
+    """
+    Crea un dataframe normalizado de las locaciones culturales presentes en los archivos CSV
+    Inputs:
+        Fecha de nacimiento
+    Ouput
+        Cantidad de años cumplidos
+    """
+    today = date.today()
+    return today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+def clean_words(column):
+    """
+    Toma una columna de un dataframe y le saca guiones, espacios y demás
+    Inputs:
+        Columna de un dataframe
+    Ouput
+        Columna normalizada
+    """
+    column = column.str.lower()
+    column = column.str.replace('mr.', '')
+    column = column.str.replace('ms.', '')
+    column = column.str.replace(' dds', '')
+    column = column.str.replace('mrs.', '')
+    column = column.str.replace(' md', '')
+    column = column.str.replace('dr.', '')
+    column = column.str.replace('_', ' ')
+    column = column.str.rstrip()
+    column = column.str.lstrip()
+    column = column.str.replace('-', '')
+    column = column.str.replace('  ', ' ')
+    return column
+
+def convert(dataframe, dict_to_location, dict_from_location):
+    """
+    Dado un dataframe, completa los datos faltantes en códigos postales y localidades
+    Inputs:
+        - Dataframe, diccionario de códigos a localidades y diccionario de localidades a códigos
+    Ouput
+        -
+    """
+    # Uso de fillna para tomar todos los casos posibles de None, NaN, null y ''
+    dataframe.location = dataframe.location.fillna(dataframe.postal_code.map(dict_to_location))
+    # Normalizamos la localidad para poder leer sin problemas del diccionario
+    dataframe['location'] = dataframe['location'].str.upper()
+    dataframe['location'] = dataframe['location'].str.replace('_', ' ')
+    dataframe['location'] = dataframe['location'].str.rstrip()
+    dataframe['location'] = dataframe['location'].str.lstrip()
+    dataframe.postal_code = dataframe.postal_code.fillna(dataframe.location.map(dict_from_location))
+    dataframe['location'] = dataframe['location'].str.lower()
+    dataframe[['first_name', 'last_name']] = dataframe['full_name'].str.split(' ', 1, expand=True)
+    # Drop las columnas no solicitadas
+    dataframe.drop('full_name', axis=1, inplace=True)
+    dataframe.drop('birth_date', axis=1, inplace=True)
+
+def pandas_process_func():
+    """
+    Toma los datos de las universidades del CSV y las normaliza a un txt
+    Inputs:
+        -
+    Ouput
+        -
+    """
+
+    # Cargamos los archivos CSV necesarios
+    DIR = os.path.dirname(__file__)
+    ARCHIVO_CSV = f'{DIR}/files/universities_a.csv'
+    ARCHIVO_VILLAMARIA = f'{DIR}/files/universidad_nacional_de_villa_maria.txt'
+    ARCHIVO_FLORES = f'{DIR}/files/universidad_de_flores.txt'
+    CODIGOS_POSTALES = f'{DIR}/files/codigos_postales.csv'
+
+    # Leemos el datagrame indicando los tipos de datoas y parseos que hacen falta
+    try:
+        dataframe = pd.read_csv(ARCHIVO_CSV, parse_dates=['birth_date'], dtype={'postal_code': str}, index_col=False)
+    except IOError as e:
+        logging.error(f'Error al leer el csv, no se lo ha encontrado: {e}')
+        raise Exception('No se encontró el archivo csv')
+    # Creamos dos diccionarios para conocer las localidades y códigos postales
+    postal_code_to_location = pd.read_csv(CODIGOS_POSTALES, header=None, index_col=0).squeeze().to_dict()
+    location_to_postal_code = pd.read_csv(CODIGOS_POSTALES, header=None, index_col=1).squeeze().to_dict()
+
+    # Limpiamos los strings de espacios, guiones, prefijos y demás
+    dataframe['university'] = clean_words(dataframe['university'])
+    dataframe['career'] = clean_words(dataframe['career'])
+    dataframe['full_name'] = clean_words(dataframe['full_name'])
+    dataframe['email'] = clean_words(dataframe['email'])
+
+    # Convertimos el género al formato pedido
+    dataframe['gender'] = dataframe['gender'].str.replace('m', 'male')
+    dataframe['gender'] = dataframe['gender'].str.replace('f', 'female')
+
+    # Convertimos la fecha de nacimiento a edad
+    dataframe['age'] = dataframe['birth_date'].apply(age)
+    # Obtenemos localidades y códigos postales
+    convert(dataframe, postal_code_to_location, location_to_postal_code)
+    # Sacamos el índice del txt final
+    dataframe.reset_index(drop=True, inplace=True)
+    dataframe_villamaria = dataframe[dataframe['university'].str.contains('universidad nacional de villa maría')]
+    dataframe_flores = dataframe[dataframe['university'].str.contains('universidad de flores')]
+    try:
+        dataframe_villamaria.to_csv(ARCHIVO_VILLAMARIA, encoding='utf-8-sig', index=False)
+        dataframe_flores.to_csv(ARCHIVO_FLORES, encoding='utf-8-sig', index=False)
+    except IOError as e:
+        logger.error(f'Error al crear el archivo TXT: {e}')
+        raise e
+
 
 default_args = {
     'retries': 1,
