01a8d3e7a78dd2624c3ce39c4f53e6012f16a624
julianpalmerio <julianpalmerio@hotmail.com>
2022-02-09 12:59:55 -0300

    Revert Python operator to a dummy operator

--- dag-universities-e.py
+++ dag-universities-e.py
@@ -58,14 +58,7 @@ with DAG(
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 1, 27)
 ) as dag:
-    extract_from_sql = PythonOperator(
-        task_id="extract_from_sql",
-        python_callable=extract,
-        # op_kwargs: Optional[Dict] = None,
-        # op_args: Optional[List] = None,
-        # templates_dict: Optional[Dict] = None
-        # templates_exts: Optional[List] = None
-    )
+    extract_from_sql = DummyOperator(task_id='extract_from_sql')
     transform_with_pandas = DummyOperator(task_id='transform_with_pandas')
     load_to_s3 = DummyOperator(task_id='load_to_s3')
 
