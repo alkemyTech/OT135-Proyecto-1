ba1c194b58f3cd8b372ed975079b69e0f81bf6c1
LaureanoEngulian <96726648+LaureanoEngulian@users.noreply.github.com>
2022-02-10 15:15:51 -0300

    Ot135 81 funcion pandas universidad h (#49)
    * .txt added to gitignore
    
    * data normalization function added
    
    * code fix
    
    * gitignore modified
    
    * loggin level change to INFO
    
    * code identation improved
    
    * code identation
    
    * try/exept added
    
    * txt creation path changed
    
    Co-authored-by: julianpalmerio <julianpalmerio@hotmail.com>

--- dag-universities-h.py
+++ dag-universities-h.py
@@ -3,7 +3,7 @@ from datetime import timedelta, datetime
 import os
 
 from airflow import DAG
-from datetime import timedelta, datetime
+from datetime import date
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 import pandas as pd
@@ -13,7 +13,7 @@ import boto3
 
 
 log.basicConfig(
-    level=log.ERROR,
+    level=log.INFO,
     format='%(asctime)s - %(module)s - %(message)s',
     datefmt='%Y-%m-%d'
 )
@@ -79,6 +79,105 @@ def load_s3(*op_args):
         except Exception as e:
             logger.error(f'Ocurrió un error: {e}')
             raise e
+
+def data_nomalization():
+    '''
+    La función lee el csv de las universidades h
+    Normaliza toda la información
+    Crea un .txt por cada una de la univeridades contenidas en el csv
+    '''
+    # Establecemos la ruta al directorio local
+    dir = os.path.dirname(__file__)
+
+    # Leemos el archivo .csv con Pandas
+    try:
+        df = pd.read_csv(f'{dir}/files/universidades-h.csv')
+    except Exception as e:
+        log.error(e)
+        raise e
+
+    # Elimino la columna unnamed del dataframe
+    df = df.drop(['Unnamed: 0'], axis=1)
+
+    # Creo una variable con las columnas no numéricas
+    columns = ['university', 'career',
+               'student_full_name', 'gender', 'location', 'email']
+
+    # Creo una iteración para las colúmnas no numérica
+    for column in columns:
+        # Convierto el dataframe a minúsculas
+        df[column] = df[column].str.lower()
+        # Reemplazo en el dataframe los "_" por un espacio
+        df[column] = df[column].replace(to_replace='-', value=' ', regex=True)
+        # Quitamos los espacios de más
+        df[column] = df[column].str.strip()
+
+    # Reemplazo valores de 'm' y 'f' por 'male' y 'female' en la columna gender
+    df['gender'] = df['gender'].replace(
+        to_replace='m', value='male', regex=True)
+    df['gender'] = df['gender'].replace(
+        to_replace='f', value='female', regex=True)
+
+    # Limpiamos la columna full_name para que nos queden solo los nombres y apellidos
+    salutations = ["mrs\.", "mr\.", "dr\.", "ms\.", "md",
+                   "dds", "dvm", "iii", "phd", "jr\.", "ii", "iv", "miss"]
+    df['student_full_name'] = df['student_full_name'].replace(
+        salutations, value='', regex=True)
+    # Separamos nombre y apellido de la columna full_name, los agregamos al df y eliminamos la columna full_name
+    name = df['student_full_name'].str.split(expand=True)
+    name.columns = ['first_name', 'last_name']
+    df = pd.concat([df, name], axis=1)
+    df = df.drop(['student_full_name'], axis=1)
+
+    # Creo una función para calcular la edad
+    def age(born):
+        born = datetime.strptime(born, "%Y-%m-%d").date()
+        today = date.today()
+        return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+    # Creo una columna con la edad de las personas
+    df['age'] = df['birth_date'].apply(age)
+    df = df.drop(['birth_date'], axis=1)
+
+    # Leemos el archivo .csv con los codigos postales y las localidades con Pandas
+    try:
+        df_cod_postales = pd.read_csv(f'{dir}/files/codigos_postales.csv')
+    except Exception as e:
+        log.error(e)
+        raise e
+    # Nos quedamos con solo un código postal por localidad
+    df_cod_postales = df_cod_postales.drop_duplicates(
+        ['localidad'], keep='first')
+    df_cod_postales['localidad'] = df_cod_postales['localidad'].str.lower()
+    # Renombramos las columnas para que coincidan con las de df
+    df_cod_postales = df_cod_postales.rename(
+        columns={'codigo_postal': 'postal_code', 'localidad': 'location'})
+    # Hago un merge de ambos dataframes para obtener localidades
+    merge_location = pd.merge(
+        df, df_cod_postales, how="left", on="postal_code")
+    merge_location.location_x = merge_location.location_x.fillna(
+        merge_location.location_y)
+    df.location = merge_location.location_x
+    # Elimino duplicados de df_codigos_postales
+    df_codigos_postales = df_cod_postales.drop_duplicates(
+        ['location'], keep='first')
+    # Hago otro merge de ambos dataframes para obtener codigos postales
+    merge_codes = pd.merge(df, df_codigos_postales, how="left", on="location")
+    merge_codes.postal_code_x = merge_codes.postal_code_x.fillna(
+        merge_codes.postal_code_y)
+    df.postal_code = merge_codes.postal_code_x
+    # Transformamos los valores de la postal_code en int
+    df['postal_code'] = df.postal_code.astype(int)
+
+    # Creamos un df filtrando por universidad de buenos aires
+    df_universidad_bs_as = df[df['university']
+                              == 'universidad de buenos aires']
+    # Exportamos el df de la universidad de buenos aires como un .txt
+    df_universidad_bs_as.to_csv(f'{dir}/files/universidad_de_buenos_aires.txt')
+    # Creamos un df filtrando por universidad del cine
+    df_universidad_cine = df[df['university'] == 'universidad del cine']
+    # Exportamos el df de la universidad de buenos aires como un .txt
+    df_universidad_cine.to_csv(f'{dir}/files/universidad_del_cine.txt')
+
 
 # instanciamos dag
 with DAG(
